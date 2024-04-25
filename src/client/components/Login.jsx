06f7f4c56c128ba6e2b0a19891c66f73a238268b
Latispq <Latispq@gmail.com>
2024-04-25 18:54:52 -0400

    Registration page styling complete. Also fixed the link to registration page on the login page.

--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -131,7 +131,7 @@ const handleSubmit = async (e) => {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/register" variant="body2">
                   Don't have an account? Sign Up
                 </Link>
               </Grid>
