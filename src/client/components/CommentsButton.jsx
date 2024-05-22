d88dfcdf578c2accbadcef575805d6ed45b155ed
Latispq <Latispq@gmail.com>
2024-05-21 22:06:19 -0400

    re-applied css to the comments and ratings box

--- src/client/components/CommentsButton.jsx
+++ src/client/components/CommentsButton.jsx
@@ -5,7 +5,7 @@ import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
 import { Block } from '@mui/icons-material';
 
-export default function CommentsButton(handleSubmit) {
+export default function CommentsButton({handleSubmit}) {
 	return (
 		<Stack direction='row' spacing={2}>
 			<Button
